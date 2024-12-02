691dfe7eafa2b900bc213df6eb1cfc2d7d0e16e7
Dmitrij Sazniov <dmitrij.sazniov@gmail.com>
2024-12-02 12:52:49 +0000

    Added filters for MatchesView with status, search, ordering. Implemented custom 404 response for no results.

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
 
     'rest_framework',
     'corsheaders',
@@ -70,6 +71,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ]
+}
 
 ROOT_URLCONF = 'api.urls'
 
--- backend/matches/views.py
+++ backend/matches/views.py
@@ -1,19 +1,33 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, filters
+from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
-from rest_framework import status
 from .models import Match
 from .serializers import MatchSerializer
 
 
-class MatchesView(APIView):
-    def get(self, request):
-        status_filter = request.query_params.get('status')
+class MatchesView(generics.ListAPIView):
+    """
+    List all matches or filter them by status and other parameters.
+    """
+    serializer_class = MatchSerializer
+    queryset = Match.objects.all().order_by('scheduled_time')
 
-        if status_filter:
-            matches = Match.objects.filter(status=status_filter).order_by('scheduled_time')
-        else:
-            matches = Match.objects.all().order_by('scheduled_time')
+    # Filtering, searching, ordering
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
 
-        serializer = MatchSerializer(matches, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    # Filtering, searching and ordering
+    filterset_fields = ['status']
+    search_fields = ['team1__name', 'team2__name', 'event__name']
+    ordering_fields = ['sheduled_time', 'status', 'event__name']
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        if not queryset.exists():
+            raise NotFound(detail="No matches found with the given criteria", code=404)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
