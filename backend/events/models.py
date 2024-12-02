8a85ede51320086622f5a198e17c443bb83d667c
Dmitrij Sazniov <dmitrij.sazniov@gmail.com>
2024-12-02 11:54:27 +0000

    Fixed models image configuration, DEBUG=TRUE MediaField - DEBUG=FALSE CloudinaryField.

--- backend/events/models.py
+++ backend/events/models.py
@@ -1,13 +1,19 @@
 from django.db import models
+from django.conf import settings
 from cloudinary.models import CloudinaryField
 
+
+if settings.DEBUG:
+    from django.db.models import ImageField as MediaField
+else:
+    MediaField = CloudinaryField
 
 class Event(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     start_date = models.DateField()
     end_date = models.DateField()
-    image = CloudinaryField('image', blank=True, null=True)
+    image = MediaField('image', blank=True, null=True)
 
     def __str__(self):
         return self.name
