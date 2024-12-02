95920c854966eac0421f1718e50c1ed4c1f4f506
Dmitrij Sazniov <dmitrij.sazniov@gmail.com>
2024-12-02 11:19:34 +0000

    Added Team model with fields for name, description and logo.

--- backend/teams/models.py
+++ backend/teams/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    logo = CloudinaryField('image', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
