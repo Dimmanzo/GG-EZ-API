60cb8ddd10517a3ca8a119bcc080e5563b129f4d
Dmitrij Sazniov <dmitrij.sazniov@gmail.com>
2024-12-02 12:16:32 +0000

    Added API view to list all matches.

--- backend/matches/views.py
+++ backend/matches/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Match
+from .serializers import MatchSerializer
 
-# Create your views here.
+
+class MatchesView(APIView):
+    def get(self, request):
+        status_filter = request.query_params.get('status')
+
+        if status_filter:
+            matches = Match.objects.filter(status=status_filter).order_by('scheduled_time')
+        else:
+            matches = Match.objects.all().order_by('scheduled_time')
+
+        serializers = MatchSerializer(matches, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
