8af6821abd97c3070a358d5e0ccbe400cd6f213d
Dmitrij Sazniov <dmitrij.sazniov@gmail.com>
2024-12-03 07:11:08 +0000

    Formatted time in the MatchSerializer.

--- backend/matches/serializers.py
+++ backend/matches/serializers.py
@@ -6,6 +6,7 @@ class MatchSerializer(serializers.ModelSerializer):
     event_name = serializers.CharField(source='event.name')
     team1_name = serializers.CharField(source='team1.name')
     team2_name = serializers.CharField(source='team2.name')
+    scheduled_time = serializers.SerializerMethodField() 
 
     class Meta:
         model = Match
@@ -13,3 +14,6 @@ class MatchSerializer(serializers.ModelSerializer):
             'id', 'event_name', 'team1_name', 'team2_name',
             'scheduled_time', 'status'
         ]
+
+    def get_scheduled_time(self, obj):
+        return obj.scheduled_time.strftime('%Y-%m-%d %H:%M:%S')
