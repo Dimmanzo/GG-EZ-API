5a7fa689f710dbaedb0949c24707c16483f9dd8f
Dmitrij Sazniov <dmitrij.sazniov@gmail.com>
2024-12-08 11:39:04 +0000

    Implemented filtering, searching, and ordering to EventListView.

--- backend/events/views.py
+++ backend/events/views.py
@@ -1,15 +1,29 @@
-from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, permissions, filters
+from rest_framework.exceptions import NotFound
 from .models import Event
 from .serializers import EventSerializer
 
 
 class EventListView(generics.ListCreateAPIView):
     """
-    List all events ordered by start date, option to create new events for admins.
+    List all events ordered by start date or create new events if logged in as admin.
     """
     serializer_class = EventSerializer
     queryset = Event.objects.all().order_by('start_date')
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    # Filtering, searching, ordering
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+
+    # Filter fields
+    filterset_fields = ['start_date', 'end_date']
+    search_fields = ['name', 'description']
+    ordering_fields = ['start_date', 'end_date']
 
     def perform_create(self, serializer):
         if not self.request.user.is_staff:
