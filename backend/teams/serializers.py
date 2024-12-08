ca94ec2645fb8ab92f32869b0c7e04135a70e9a7
Dmitrij Sazniov <dmitrij.sazniov@gmail.com>
2024-12-08 12:37:10 +0000

    Added players to the Team view.

--- backend/teams/serializers.py
+++ backend/teams/serializers.py
@@ -1,10 +1,6 @@
 from rest_framework import serializers
 from .models import Team, Player
 
-class TeamSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Team
-        fields = ['id', 'name', 'description', 'logo']
 
 class PlayerSerializer(serializers.ModelSerializer):
     team_name = serializers.CharField(source='team.name', read_only=True)
@@ -12,3 +8,10 @@ class PlayerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Player
         fields = ['id', 'name', 'role', 'avatar', 'team', 'team_name']
+
+class TeamSerializer(serializers.ModelSerializer):
+    players = PlayerSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Team
+        fields = ['id', 'name', 'description', 'logo', 'players']
