fa803ac351de2c6305b2f52e9a60aecf6fb906fd
Dmitrij Sazniov <dmitrij.sazniov@gmail.com>
2024-12-08 11:54:04 +0000

    Updated EventDetailView with delete and edit functionality and admin-only permissions.

--- backend/events/views.py
+++ backend/events/views.py
@@ -1,6 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import generics, permissions, filters
+from rest_framework import generics, permissions, filters, status
 from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
 from .models import Event
 from .serializers import EventSerializer
 
@@ -30,15 +31,34 @@ class EventsView(generics.ListCreateAPIView):
             raise permissions.PermissionDenied("Only admins can create events.")
         serializer.save()
 
-class EventDetailView(generics.RetrieveAPIView):
+class EventDetailView(generics.RetrieveUpdateDestroyAPIView):
     """
     Show details of a specific event by ID.
     """
     serializer_class = EventSerializer
     queryset = Event.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get_object(self):
         try:
             return self.queryset.get(pk=self.kwargs['pk'])
         except Event.DoesNotExist:
             raise NotFound(detail="No event found with the given ID.", code=404)
+
+    def delete(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if not request.user.is_staff:
+            return Response(
+                {"detail": "Only admins can delete event."},
+                status=status.HTTP_403_FORBIDDEN
+            )
+        obj.delete()
+        return Response({"detail": "Event deleted succesfully."}, status=status.HTTP_204_NO_CONTENT)
+
+    def perform_create(self, serializer):
+        if not self.request.user.is_staff:
+            return Response(
+                {"detail": "Only admins can update events."},
+                status=status.HTTP_403_FORBIDDEN
+            )
+        serializer.save()
